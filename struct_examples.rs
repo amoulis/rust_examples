131ad030db7be72095ce122cbf7d942a4d76d930
amoulis <amoulis>
2022-01-31 18:47:15 +0100

    Tuples and tuples printing

--- struct_examples.rs
+++ struct_examples.rs
@@ -38,4 +38,16 @@ fn main() {
    
    println!("Hello, world {}!", batman);
    println!("Hello, world {}!", MightiestHeroes);
+
+
+   // Tuple Manipulation
+   let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                      -1i8, -2i16, -3i32, -4i64,
+                      0.1f32, 0.2f64,
+                      'a', true);
+
+    // Values can be extracted from the tuple using tuple indexing
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.1);
+    println!("tuple of tuples: {:?}", long_tuple);
 }
